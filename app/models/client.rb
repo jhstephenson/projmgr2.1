886f94d45994dba7caed403221cae0a5be73a399
Prelang Builder <builder@prelang.com>
2014-11-13 18:26:11 +0000

    Defined validation for attribute 'contact_phone' on model 'Client'

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,5 @@ class Client < ActiveRecord::Base
   has_many :projects
   validates_formatting_of :zip_code, using: :us_zip
   validates_formatting_of :contact_email, using: :email
+  validates_formatting_of :contact_phone, using: :us_phone
 end
